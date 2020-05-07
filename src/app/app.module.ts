398ee1a3a67ce1a8235831f00b5805c40f706c2a
milimolo <bondemand@hotmail.dk>
2020-05-07 10:51:37 +0200

    bam

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,9 @@ import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { SateliteListComponent } from './satelite/satelite-list/satelite-list.component';
 import { SateliteInfoComponent } from './satelite/satelite-info/satelite-info.component';
+import {environment} from '../environments/environment';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAAVgyiaP2VjsSktf9Xk8ooRhtFIpiU-nA',
-  authDomain: 'satelite-database.firebaseapp.com',
-  databaseURL: 'https://satelite-database.firebaseio.com',
-  projectId: 'satelite-database',
-  storageBucket: 'satelite-database.appspot.com',
-  messagingSenderId: '112771841346',
-  appId: '1:112771841346:web:992bbbeba620f6ede85cdb',
-  measurementId: 'G-D73H65QJT6'
-};
+
 
 @NgModule({
   declarations: [
@@ -29,7 +21,7 @@ const firebaseConfig = {
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(environment.firebaseConfig)
 
   ],
   providers: [],
