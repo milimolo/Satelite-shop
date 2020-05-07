9cb5ed31e81999224689394c7563da1a04a771e3
Frederik Jensen <kungfu15447@users.noreply.github.com>
2020-05-07 11:34:32 +0200

    Update satelite.service.ts

--- src/app/Satelite/shared/satelite.service.ts
+++ src/app/Satelite/shared/satelite.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SateliteService {
 
-  constructor() { }
+  constructor(afs: AngularFirestore) { }
 }
