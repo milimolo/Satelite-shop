2b54778ff6d5e34ac56ccb58c90b8556affd7723
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2020-05-15 12:55:19 +0200

    it is working

--- src/app/Products/Fuel/module/fuel-routing.module.ts
+++ src/app/Products/Fuel/module/fuel-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {FuelCreateComponent} from '../fuel-create/fuel-create.component';
 import {FuelUpdateComponent} from '../fuel-update/fuel-update.component';
+import {FuelListComponent} from '../fuel-list/fuel-list.component';
 
 
 const routes: Routes = [
+  {
+    path: '',
+    component: FuelListComponent
+  },
   {
     path: 'create',
     component: FuelCreateComponent
