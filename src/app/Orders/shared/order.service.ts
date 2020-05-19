ea94edf31c51071e3f43f1d7038f80a051a0c93b
Frederik Jensen <kungfu15447@users.noreply.github.com>
2020-05-19 15:20:42 +0200

    Update order.service.ts

--- src/app/Orders/shared/order.service.ts
+++ src/app/Orders/shared/order.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {AngularFirestore} from '@angular/fire/firestore';
-import {Order} from "./order.model";
+import {Order} from './order.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,6 @@ export class OrderService {
   constructor(private afs: AngularFirestore) { }
 
   createOrder(order: Order) {
-    
+
   }
 }
